041e43b39623c581848d76f1658c2bd1ce9b9aa9
nmousseau <36670797+nmousseau@users.noreply.github.com>
2018-04-09 12:05:56 -0400

    Add files via upload

---
+++ realgame.py
@@ -0,0 +1,108 @@
+print("Welcome to Game")
+print("Type quit at any time to quit")
+print("Type go to continue")
+start = 'go'
+replay = 'play again'
+something = 'yes'
+Ahern = 'Ahern'
+McDonalds = 'McDonalds'
+Leavitt = 'Leavitt Park'
+Fratellos = 'Fratellos'
+Garage = 'Parking Garage'
+Aromas = 'Aroma Joes'
+Wash = 'Car Wash'
+Tressel = 'Tressel'
+Belknap = 'Belknap Mill'
+Library = 'Library'
+Chase = 'Chase'
+Track = 'Track'
+Skate = 'Skate Escape'
+belknap = 'Belknap Mall'
+Wow = 'Wow Trail'
+Orthodontist = 'Orthodontist'
+Home = 'Home'
+Theatre = 'Colonial Theatre'
+Pine = 'Pine Street'
+Eddie = 'Eddies House'
+Braces = 'Braces'
+Police = 'Police'
+Make = 'Make'
+Watch = 'Watch'
+game = True
+
+
+while game:
+    ans = input("Type \n")
+    if ans == "quit":
+        game = False
+
+    if start in ans:
+        print('You find yourself boolin with the homies. They wanna go chill somewhere else. Do you want to go hang out at Ahern or McDonalds?')            
+    if replay in ans:
+        print('You find yourself boolin with the homies. They wanna go chill somewhere else. Do you want to go hang out at Ahern or McDonalds?')
+    if Ahern in ans:
+        print('You decide to go to Ahern. While boolin wit the homies at Ahern, you get scared by the pedophile prison nearby. You all decide to leave. Do you want to go to Funspot or Memorial Park?')
+    if McDonalds in ans:
+         print('You decide to go to McDonalds. While boolin wit the homies at McDonalds, the LHS valedictorian from 2016 kicks you out for not buying anything. Pissed off, your homies decide to leave after squirting mayonnaise on his face. Do you want to go to Leavitt Park or Fratellos?')
+    if Leavitt in ans:
+        print('You decide to go with your homies to Leavitt Park. When you get there, you realize your friend has given you a slight erection and you are forced to masturbate violently. You try to contain the cum in your hand, but some gets on your friends shirt. He is super mad, and threatens to beat you up. You call for a truce, but accidentally shake his hand with yours which is filled with hot cum. After both of you swallow cum, you need to clean up. Are you going to the car wash or the tressel?')
+    if Fratellos in ans:
+        print('You and your homies get to Fratellos. After ordering a platter of coleslaw, your waiter comes back and sits on your lap. Apparently you had been giving him signs that you were DTF. Confused about your sexuality, you decide you have to leave, and convince your friends too. Are you going to go to the Parking Garage or Aroma Joes?')
+    if Garage in ans:
+        print('You decide to go to the Parking Garage. You see a lamborghini, and decide to take a piss on your friend. Your friend pushes you off the top floor of the parking garage and you land in a pothole. You died. Type quit to quit or play again to play again')
+    if Aromas in ans:
+        print('You decide to order a coffee for you and your homies to share. It tastes good. One of your homies gets a call inviting him to a party at the Belknap Mill. But, you would rather go to the Library. Are you gonna go to the Belknap Mill or the Library?')
+    if Wash in ans:
+        print('You head to the Car Wash to clean off. One of your friends gets soap in his eye and goes blind. He wanders out into the street where a passing car stops, grabs him and drives off. But that friend has autism, so do you want to go after him really or would you rather go to the Track? Chase or Track?')
+    if Tressel in ans:
+        print('All of you head to the Tressel to wash off. You jump in one by one, then get out and push the car into the lake to clean it off. Unfortunately, you cant retrieve it and are forced to leave on foot. Are you going to Skate Escape or the Belknap Mall?')
+    if Belknap in ans:
+        print('You all go to the party at the Belknap Mill. One of your friends snorts cocaine, but turns out it was actually dog shit. He starts dying, so you have to get him some help. Do you want to bring him to the homeless shelter on the Wow Trail or bring him to the Orthodontist?')
+    if Library in ans:
+        print('You get to the Library to try and check out Harry Potter and the Gay. Unfortunately, it is already checked out. You wrap yourself up in wires and hang yourself from the staircase. Pussy. Type quit to quit or play again to play again')
+    if Chase in ans:
+        print('You chase after your friend at 10mph. The car takes lots of turns, and stops at the high school. You watch from a distance. Out of the car steps Phill Reed. You run up to him as he is raping your blind friend. He asks if you want to join and you say yes. Type quit to quit or play again to play again')
+    if Track in ans:
+        print('You leave your autistic and now blind friend behind in the car. You instead go to the Track to run some laps. You get extremely tired after 27 laps. Your friends want to go home, but you would rather go to the Colonial Theatre. Are you going Home or to the Colonial Theatre?')
+    if Skate in ans:
+        print('You and your homies go to Skate Escape. When you get there, you realize it burned down. Out of depression and sexual longing, you all kill yourselves. Type quit to quit or play again to play again')
+    if belknap in ans:
+        print('You go to the Belknap Mall with your homies to pump shit up. Your friends want to go to the coke house on Pine Street, but you would rather go to Eddies House. Are you going Pine Street or Eddies House?')
+    if Wow in ans:
+        print('You bring your friend who snorted dog shit to the Wow Trail to be treated by Joey T. Joey T diagnoses him as paralysed and you leave him with Joey T. Then you kill yourself. Type quit to quit or play again to play again')
+    if Orthodontist in ans:
+        print('You bring your friend to the Orthodontist to get the dog shit out of his nose. You can stay and get braces or go to the Fuck 12 Station. Braces or Station?')
+    if Home in ans:
+        print('You and your pussy friends go home and when you get home you die in your sleep from swallowing a wild alligator. Type quit to quit or play again to play again')
+    if Theatre in ans:
+        print('You go to the Colonial Theatre to watch a movie with your homies. You had a cool plan to sneak in without tickets, but you get there and realize the Colonial Theatre has been closed for 20 years. You can go watch a porno or make a porno. Watch or Make?')
+    if Pine in ans:
+        print('You go the Pine Street to become a gangster at a coke house. You become a blood and your friend becomes a crip. All of the gang members in Laconia make you fight to the death. And you lose. Type quit to quit or play again to play again')
+    if Eddie in ans:
+        print('You go to Eddies House where you get eaten by Raymond. Type quit to quit or play again to play again')
+    if Braces in ans:
+        print('Type quit to quit or play again to play again')
+    if Police in ans:
+        print('You go to the Fuck 12 Station, where you scream Fuck 12 with your phone held in the air. The police mistake your phone for a gun and shoot your black friend. You are then arrested for promoting gang affiliation. In jail you are raped by Caleb Hounsell and Dirty Dirty ODonnell. Type quit to quit or play again to play again')
+    if Make in ans:
+        print('You and your homies Make an ass eating porno. You make millions of dollars and live at Pornhub studios in Manchester, New Hampshire. You win the game. Type quit to quit or play again to play again')
+    if Watch in ans:
+        print('You and your homies Watch a porno in 144p and it turns out it was gay, not straight. You and your homies have an orgy, then realize you forgot to say no homo. Type quit to quit or play again to play again')
+    if something in ans:
+        print('found')
+        
+"""
+i = 0
+j = 0
+
+
+    while i < 5:
+        print("NEW ROW")
+        while j < 5:
+            print("x")
+            j += 1
+        j = 0
+        i += 1
+"""
+#
+
